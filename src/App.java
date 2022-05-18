2ce76e717c26f6b4192ec260a62b520a078b0ed5
Gilson Damaso <gilson.damaso@gmail.com>
2022-05-18 09:22:25 -0300

    MATRIZ_SIMPLES

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("INFORME O TAMANHO DA MATRIZ: ");
+        int n = sc.nextInt();
+
+        int[][] numeros = new int[n][n];
+
+        for (int i = 0; i < numeros.length; i++) {
+            for (int j = 0; j < numeros.length; j++) {
+                numeros[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("DIAGONAL PRINCIPAL: ");
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print(numeros[i][i] + " ");            
+        }
+
+
+        sc.close();
+
     }
 }
