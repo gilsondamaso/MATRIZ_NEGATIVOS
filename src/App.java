5761e0db2adfa2d6cc5289c46a0e6d13ce5f53bc
Gilson Damaso <gilson.damaso@gmail.com>
2022-05-18 09:25:40 -0300

    quantidade negativos

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-
+        int count = 0;
         System.out.println("INFORME O TAMANHO DA MATRIZ: ");
         int n = sc.nextInt();
 
@@ -20,7 +20,14 @@ public class App {
             System.out.print(numeros[i][i] + " ");            
         }
 
-
+        for (int i = 0; i < numeros.length; i++) {
+            for (int j = 0; j < numeros.length; j++) {
+                if (numeros[i][j] < 0 ) {
+                  count++;  
+                }
+            }
+        }
+        System.out.println("QTD NEGATIVOS: " + count);
         sc.close();
 
     }
